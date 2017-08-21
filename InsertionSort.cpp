aa846ddf3bc19edc7ef7613ffb8d687fa332d1f6
Francesco <Francesco325246247@users.noreply.github.com>
2017-08-21 20:04:36 +0200

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,22 @@
+/*
+Insertion Sorting is an efficient algorithm to sort a SMALL number of elements in a set.
+
+PSEUDO-CODE
+
+A = <3,6,7,5,4,8,1,9,2>
+
+for j = 2 to A.lenght
+  key = A[j]
+  i = j - 1
+  while i>0 and A[i] > key
+  i = i - 1
+ A[i+1] = key
+ 
+ for j = 2 to A.lenght
+  key = A[j]
+  i = j - 1
+  while i>0 and A[i] > key
+  i = i - 1
+ A[i+1] = key
+
+*/
