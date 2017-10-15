237a9710c72984870397606e4070804beca0ce1d
Francesco <Francesco325246247@users.noreply.github.com>
2017-10-15 17:59:31 +0200

    Update quicksorting.cpp

--- quicksorting.cpp
+++ quicksorting.cpp
@@ -1,69 +1,43 @@
-#include <iostream>
-#include<vector>
-#define  forv(a,b) for(int i = (a); i<(b); ++i)
-#define pivot(left,right) ((left+right)/2)
-
-typedef vector<int> v;
+#include<iostream>
 
 using namespace std;
 
-void swap(v victor, int left, int right) {
-    int temp = victor[right];
-    victor[right] = victor[left];
-    victor[left] = temp;
+int Partition(int vector1[], int start, int end) {
+    int startb = start, pivot = vector1[start], index;
+
+    for (index = start + 1; index <= end; index++) {
+        if (pivot > vector1[index]) {
+            vector1[startb] = vector1[index];
+            vector1[index] = vector1[startb + 1];
+            vector1[startb + 1] = pivot;
+            startb = startb + 1;
+        }
+    }
+    return startb;
 }
 
-v mergeVector(v victor1, v victor2){
-    v megaVictor;
-    for(int i = 0; i < victor1.size(); ++i){
-        megaVictor[i] = victor1[i];
-    }
-    for(int i = 0; i < victor2.size(); ++i){
-        megaVictor[i] = victor2[i];
-    }
-    return megaVictor;
-}
-v splitLeftVector(v victor, int pivot_value, int left, int right) {
-    v left_victor;
-    for (int i = left; i < pivot_value; ++i) {
-        if (victor[i] < victor[pivot_value]) {
-            left_victor[i] = victor[i];
-        }
-        return left_victor;
+void print(int vector1[], int size) {
+    for (int i = 0; i < size; ++i) {
+        cout << "[" << vector1[i] << "]";
     }
 }
-v splitRightVector(v victor, int pivot_value, int left, int right) {
-    v right_victor;
-    for (int i = pivot_value; i < right; ++i) {
-        if (victor[i] > victor[pivot_value]) {
-            right_victor[i] = victor[i];
-        }
-        return right_victor;
+
+void QuickSort(int vector1[], int start, int end) {
+    if (start < end) {
+        int p = Partition(vector1, start, end);
+        QuickSort(vector1, start, p - 1);
+        QuickSort(vector1, p + 1, end);
     }
-}
-v quicksort(v victor, int left, int right) {
-    v leftv;
-    v rightv;
-    forv(left,victor.size()){
-        if(victor[left]>victor[left+1]){
-            if(victor[left]>victor[left+1] && left <= pivot(left,right)) {
-                leftv = splitLeftVector(victor, pivot(left, right), left, right);
-                quicksort(leftv, left, right);
-            }
-            if(victor[left]>victor[left+1] && left > pivot(left,right)){
-                 rightv = splitLeftVector(victor, pivot(left, right), left, right);
-                quicksort(rightv, left, right);
-            }
-            
-        }
-        
-    }
-    return mergeVector(leftv,rightv);
+
 }
 
 int main() {
-    vector<int>myvect ={9,3,6,2};
-    quicksort(myvect,0,4);
+    int vector1[] = {55, 44, 2, 1, 65};
+    int start = 0, end = 4;
+
+    QuickSort(vector1, start, end);
+
+    print(vector1, end);
+
     return 0;
 }
-
